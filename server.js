2be5e343059c6f220a94329c653a27f21b50399b
alexm <alex-m@outlook.com>
2023-04-21 14:42:36 -0700

    Specify host to connect to

--- server.js
+++ server.js
@@ -2,6 +2,6 @@
 
 const app = require("./app");
 
-app.listen(3000, function() {
-  console.log("listening on 3000");
+app.listen(3000, "127.0.0.1", function() {
+  console.log("listening on host 127.0.0.1, port 3000");
 });
