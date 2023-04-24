319b43afd42b9c486becb7a6085aa49b4b44686a
alexm <alex-m@outlook.com>
2023-04-24 09:29:48 -0700

    Minor formatting edits - indentation, destructuring on imports, spacing

--- db.js
+++ db.js
@@ -4,9 +4,8 @@
 /** Database for lunchly */
 
 const pg = require("pg");
-
 const db = new pg.Client("postgresql:///lunchly");
 
 db.connect();
 
-module.exports = db;
+module.exports = { db };
--- server.js
+++ server.js
@@ -7,5 +7,5 @@ const app = require("./app");
 
 
 app.listen(3000, "127.0.0.1", function() {
-  console.log("listening on host 127.0.0.1, port 3000");
+    console.log("Listening on host 127.0.0.1, port 3000");
 });
