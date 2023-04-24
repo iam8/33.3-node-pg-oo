e304f5aca0fc3bcab7baa436a438d43563f43a14
alexm <alex-m@outlook.com>
2023-04-24 13:35:46 -0700

    Write method to save a new reservation to database

--- models/reservation.js
+++ models/reservation.js
@@ -38,6 +38,27 @@ class Reservation {
 
         return results.rows.map(row => new Reservation(row));
     }
+
+    /** Save this reservation. */
+    async save() {
+
+        if (this.id === undefined) {
+
+            // Saving a new reservation
+            const result = await db.query(`
+                INSERT INTO reservations (customer_id, start_at, num_guests, notes)
+                    VALUES ($1, $2, $3, $4)
+                    RETURNING id`,
+                [this.customerId, this.startAt, this.numGuests, this.notes]);
+
+            this.id = result.rows[0].id;
+
+        } else {
+
+            // Saving changes to an existing reservation
+
+        }
+    }
 }
 
 
